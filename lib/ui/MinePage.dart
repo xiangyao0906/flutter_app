bf483999d2c22715854a761129099ef626ace58a
xiangyao <xiangyao@hooyue.com>
2019-08-29 19:39:50 +0800

    ...

--- lib/ui/MinePage.dart
+++ lib/ui/MinePage.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+final List<String> entries = <String>['A', 'B', 'C'];
+final List<int> colorCodes = <int>[600, 500, 100];
+
 class ThirdPage extends StatefulWidget {
   @override
   ThirdPageState createState() => new ThirdPageState();
@@ -10,9 +13,26 @@ class ThirdPageState extends State<ThirdPage> {
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
-      appBar: new AppBar(title: new Text("我的"),centerTitle: true,),
-      body: new Center(
-        child: new Text("我的"),
+      appBar: new AppBar(
+        title: new Text("我的"),
+        centerTitle: true,
+      ),
+      body: new Column(
+        children: <ListView>[
+          ListView.separated(
+            padding: const EdgeInsets.all(8.0),
+            itemCount: entries.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                height: 50,
+                color: Colors.amber[colorCodes[index]],
+                child: Center(child: Text('Entry ${entries[index]}')),
+              );
+            },
+            shrinkWrap: true,
+            separatorBuilder: (BuildContext context, int index) => const Divider(),
+          )
+        ],
       ),
     );
   }
