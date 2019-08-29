61b3d66985ca5e17858a0d6912348a0179e471f1
xiangyao <xiangyao@hooyue.com>
2019-08-29 16:33:15 +0800

    ...

--- lib/ui/IdeaPage.dart
+++ lib/ui/IdeaPage.dart
@@ -10,10 +10,21 @@ class SencondPageState extends State<SencondPage> {
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
-      appBar: new AppBar(title: new Text("想法"),centerTitle: true,),
-      body: new Center(
-        child: new Text("想法"),
-      ),
-    );
+        appBar: new AppBar(
+          title: new Text("想法"),
+          centerTitle: true,
+        ),
+        body: new Column(
+          children: <Widget>[
+            Text('Deliver features faster'),
+            Text('Craft beautiful UIs'),
+            Expanded(
+              child: FittedBox(
+                fit: BoxFit.contain, // otherwise the logo will be tiny
+                child: const FlutterLogo(),
+              ),
+            ),
+          ],
+        ));
   }
 }
